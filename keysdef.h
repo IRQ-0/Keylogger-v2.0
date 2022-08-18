bd8c423f2925a9881a627882b9e12331d71edd91
IRQ0 <maciej.maloczok@gmail.com>
2022-08-19 01:10:06 +0200

    Special keys definitions

---
+++ keysdef.h
@@ -0,0 +1,48 @@
+#ifndef KEYSDEF_H
+#define KEYSDEF_H
+
+#define RANGE_LOW 32
+#define RANGE_HI 126
+
+#define SPECIAL 1
+#define NORMAL 2
+
+#define CTRL_LEFT 162
+#define CTRL_RIGHT 163
+#define TAB 9
+#define BACKSPACE 8
+#define ESC 27
+#define SHIFT_LEFT 160
+#define SHIFT_RIGHT 161
+#define WINDOWS 92
+#define CAPSLOCK 20
+#define ENTER 13
+#define LEFT_ARROW 37
+#define RIGHT_ARROW 39
+#define UP_ARROW 38
+#define DOWN_ARROW 40
+#define INSERT 45
+#define DELETE 46
+#define HOME 36
+#define END 35
+#define PAGE_UP 33
+#define PAGE_DOWN 34
+#define PAUSE 19
+#define SCROLL_LOCK 145
+#define PRINTSCREEN 44
+#define NUMLOCK 144
+
+#define F1 112
+#define F2 113
+#define F3 114
+#define F4 115
+#define F5 116
+#define F6 117
+#define F7 118
+#define F8 119
+#define F9 120
+#define F10 121
+#define F11 122
+#define F12 123
+
+#endif
